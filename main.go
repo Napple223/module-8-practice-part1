236eae004764fdc20bb8cd10d424b1412dfaab24
Самсонов Андрей <napple223@gmail.com>
2023-02-12 17:54:31 +0400

    дописал пакет main, чтобы он выводил операционную систему и количество кнопок

--- main.go
+++ main.go
@@ -7,16 +7,24 @@ import (
 
 func main() {
 	i := electronic.NewApplePhone("", "Iphone 14", "", "")
-	//a := electronic.NewAndroidPhone("Samsung", "A70", "", "Android")
-	//r := electronic.NewRadioPhone("Alcatel", "XP-55", "station", 12)
+	a := electronic.NewAndroidPhone("Samsung", "A70", "", "Android")
+	r := electronic.NewRadioPhone("Alcatel", "XP-55", "station", 12)
 	printCharacteristics(&i)
-	//printCharacteristics(&a)
+	pc1(&i)
+	printCharacteristics(&a)
+	pc1(&a)
+	printCharacteristics(&r)
+	pc2(&r)
 }
 
 func printCharacteristics(p electronic.Phone) {
 	fmt.Printf("Бренд:%v\nМодель:%v\nТип:%v\n", p.Brand(), p.Model(), p.Type())
 }
 
-func pr(s electronic.Smartphone) {
+func pc1(s electronic.Smartphone) {
+	fmt.Printf("Операционная система:%s\n", s.OS())
+}
 
+func pc2(sp electronic.StationPhone) {
+	fmt.Printf("Количество кнопок:%v\n", sp.ButtonsCount())
 }
